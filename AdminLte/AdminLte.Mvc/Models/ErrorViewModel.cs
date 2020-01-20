e036902f6cb074e5f61883461e6646f0223a25ed
Ismail Yilmaz <iyilm4z@gmail.com>
2020-01-20 11:23:53 +0300

    solution created

---
+++ AdminLte/AdminLte.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdminLte.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
